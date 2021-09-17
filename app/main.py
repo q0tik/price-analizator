700a8b3f39bc6b474d0b8412ee1ac19045371642
q0tik <q0tik@mail.ru>
2021-09-17 19:23:43 +0300

    mvp

--- app/main.py
+++ app/main.py
@@ -1,7 +1,51 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import pickle
+import pandas as pd
+import numpy as np
+from geopy.geocoders import Nominatim
 
-app = Flask(__name__)
+geolocator = Nominatim(user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15")
+app = Flask(__name__, template_folder='./static/templates')
+columns = ['title', 'year', 'mileage', 'engine_capacity', 'hp', 'fuel_type', 'gearbox', 'carbody', 'city', 'transmission', 'color']
+cols = ['wallsMaterial', 'floorNumber', 'floorsTotal', 'totalArea', 'kitchenArea', 'latitude', 'longitude']
+model = pickle.load(open('app/static/model/finalized_model.sav', 'rb'))
+model_houses = pickle.load(open('app/static/model/finalized_model_houses.sav', 'rb'))
 
 @app.route('/')
 def index():
-    return '<h1>meow</h1>'
+    return render_template('index.html')
+
+@app.route('/prediction_auto', methods=['POST'])
+def prediction_auto():
+    print(request.form.values())
+
+    init_features = [x for x in request.form.values()]
+    print(init_features)
+
+    final = list(map(float, init_features[:12]))
+    df = pd.DataFrame([final], columns = columns)
+
+    prediction = f"{np.round(np.e**model.predict(df))}".replace('[', '').replace(']', '') + ' рублей'
+
+    return render_template('result.html', pred=f'{prediction}')
+
+@app.route('/prediction_house', methods=['POST'])
+def prediction_house():
+    print(request.form.values())
+
+    init_features = [x for x in request.form.values()]
+    location = geolocator.geocode(init_features[-1])
+    init_features = init_features[:-1]
+    init_features.append(f"{location.latitude}")
+    init_features.append(f"{location.longitude}")
+    print(init_features)
+    print(len(init_features))
+
+    final = list(map(float, init_features[:7]))
+    df = pd.DataFrame([final], columns = cols)
+    
+    print(df)
+    print('---------------')
+    prediction = f"{np.round(np.e**model_houses.predict(df))}".replace('[', '').replace(']', '') + ' рублей'
+
+    return render_template('result.html', pred=f'{prediction}')
---
+++ app/train.py
@@ -0,0 +1,53 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+import glob
+import pickle
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+#import statsmodels.api as sm
+#from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+PATH = './static/csv/'
+
+def get_csv_files(path_dir):
+    return glob.iglob(f"{path_dir}*.csv", recursive=False)
+
+last = ''
+csvName = get_csv_files(PATH)
+for filE in csvName:
+    last = filE
+#%matplotlib inline
+
+#2021-09-15_13:45:21.251410_cars.csv
+# csvfile = open('2021-09-15_13:45:21.251410_cars.csv', encoding='utf8')
+# reader = csv.reader(csvfile, delimiter=';')
+columns = ['title', 'rub_price', 'year', 'mileage', 'engine_capacity', 'hp', 'fuel_type', 'gearbox', 'carbody', 'city', 'transmission', 'color']
+# data = pd.DataFrame(data=csvfile, columns=columns)
+data = pd.read_csv(last, usecols=columns, sep=';')
+# data.head() # some top data
+df = pd.DataFrame(data.dropna(), columns=columns)
+
+prices = np.log(df['rub_price'])
+features = df.drop('rub_price', axis=1)
+
+xtr, xte, ytr, yte = train_test_split(features, prices, test_size=0.2, random_state=10)
+
+
+MODEL_NAME = './static/model/finalized_model.sav'
+regr = pickle.load(open(MODEL_NAME, 'rb'))
+
+regr.fit(xtr, ytr)
+print("training data r-squared: ", regr.score(xtr, ytr))
+print("test data r-squared: ", regr.score(xte, yte))
+
+print('intercept ', regr.intercept_)
+pd.DataFrame(data=regr.coef_, index=xtr.columns, columns=['coef'])
+
+pickle.dump(regr, open(MODEL_NAME, 'wb'))
